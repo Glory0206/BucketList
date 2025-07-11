20c34f1e1630c9b801af0e846484e28328670f11
Yeonggwang <dmb07223@gmail.com>
2025-07-12 04:22:35 +0900

    feat: User, BucketList entity 생성

--- src/main/java/com/bucketlist/app/domain/BucketItem.java
+++ src/main/java/com/bucketlist/app/domain/BucketItem.java
@@ -1,4 +1,26 @@
 package com.bucketlist.app.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor // 기본 생성자 생성
+@AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자 생성
+@Builder
+@Table(name = "bucket_item")
 public class BucketItem {
+
+    @Id // 기본키임을 보여주는 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private boolean completed;
+
+    private LocalDate dueDate;
 }
