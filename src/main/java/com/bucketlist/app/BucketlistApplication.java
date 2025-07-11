b0d64ae25d26b2bca6a058d7c42a1299d0390ad2
Yeonggwang <dmb07223@gmail.com>
2025-07-12 02:36:12 +0900

    chore: 초기 프로젝트 세팅 및 PostgreSQL 연동 설정

---
+++ src/main/java/com/bucketlist/app/BucketlistApplication.java
@@ -0,0 +1,11 @@
+package com.bucketlist.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketlistApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BucketlistApplication.class, args);
+    }
+}
