5b08e8e5cedab185989202f81b6a364b0569da10
Yeonggwang <dmb07223@gmail.com>
2025-07-12 17:23:44 +0900

    feat: Bucketlist와 User간의 관계 형성

--- src/main/java/com/bucketlist/app/domain/BucketItem.java
+++ src/main/java/com/bucketlist/app/domain/BucketItem.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @NoArgsConstructor // 기본 생성자 생성
 @AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자 생성
 @Builder
-@Table(name = "bucket_item")
+@Table(name = "bucket-item")
 public class BucketItem {
 
     @Id // 기본키임을 보여주는 어노테이션
@@ -23,4 +23,8 @@ public class BucketItem {
     private boolean completed;
 
     private LocalDate dueDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
