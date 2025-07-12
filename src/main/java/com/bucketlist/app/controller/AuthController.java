f2f4f43af72e70c5215159d93acc4f7bde0d542d
Yeonggwang <dmb07223@gmail.com>
2025-07-12 19:16:40 +0900

    feat: login 기능 구현 및 테스트 완료

--- src/main/java/com/bucketlist/app/controller/AuthController.java
+++ src/main/java/com/bucketlist/app/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.bucketlist.app.controller;
 
+import com.bucketlist.app.domain.User;
+import com.bucketlist.app.dto.UserLoginRequest;
 import com.bucketlist.app.dto.UserSignupRequest;
+import com.bucketlist.app.repository.UserRepository;
+import com.bucketlist.app.security.JwtTokenProvider;
 import com.bucketlist.app.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor // final 선언된 필드 자동 생성자 주입
 public class AuthController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody @Valid UserSignupRequest request){
         userService.signup(request);
         return ResponseEntity.ok("회원가입 완료");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid UserLoginRequest request){
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 이메일입니다."));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String token = jwtTokenProvider.createToken(user.getEmail());
+
+        return ResponseEntity.ok().body(token); // fornt가 token을 저장하여 사용
     }
 }
